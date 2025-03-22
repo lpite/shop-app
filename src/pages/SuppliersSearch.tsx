cef5be98e174d68eb2a1e8b257cd59676ae20300
lpite <farionsasa@gmail.com>
2025-03-22 16:51:46 +0200

    supplier search page

--- src/pages/SuppliersSearch.tsx
+++ src/pages/SuppliersSearch.tsx
@@ -1,16 +1,30 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
+import { useProducts } from "../hooks/useProducts";
+import { useAppStore } from "../stores/useAppStore";
+import { useLocation, useSearchParams } from "wouter";
+import { useSWRConfig } from "swr";
+
+type SupplierProduct = {
+	supId: string;
+	article: string;
+	name: string;
+	description: "";
+	price: string;
+	photo: string;
+	avaible: boolean;
+	brand: string;
+	currency_name: string;
+	stocks: Record<string, { quantity: string | number; price: number }>;
+	quantityKhm: string | number;
+};
 
 export default function SuppliersSearch() {
-	const [searchValue, setSearchValue] = useState("");
-	const [items, seItems] = useState<
-		{
-			uuid: string;
-			name: string;
-			article: string;
-			brand: string;
-			default_image: string;
-		}[]
-	>([]);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [searchValue, setSearchValue] = useState(searchParams.get("q") || "");
+	const [items, seItems] = useState<SupplierProduct[]>([]);
+	const { mutate } = useSWRConfig();
+
+	const _ = useProducts({});
 
 	async function search(e: FormEvent) {
 		e.preventDefault();
@@ -24,33 +38,23 @@ export default function SuppliersSearch() {
 			alert("no 'pb_hook_url' in localStorage");
 			return;
 		}
-		const result = (await fetch(`${pb_hook_url}${searchValue}`)
+		mutate("/products/");
+
+		const result = (await fetch(
+			`${pb_hook_url}${searchValue.replace(/\s+/g, "")}`,
+		)
 			.then((res) => res.json())
 			.catch((err) => {
-				console.log(err);
+				console.error(err);
 				return null;
-			})) as {
-			headers: any;
-			products: {
-				article: string;
-				avaible: boolean;
-				brand: string;
-				currency_name: string;
-				default_image: string;
-				name: string;
-				price: string;
-				uuid: string;
-			}[];
-		};
-		if (result?.products) {
-			console.log(result?.products);
-			seItems(result?.products);
-			// setFilteredProducts(result.items);
+			})) as SupplierProduct[];
+		if (result) {
+			seItems(result);
 		}
 	}
 
 	return (
-		<main className="py-4 flex flex-col items-center">
+		<main className="py-4 flex flex-col items-center w-full">
 			<div className="fixed top-0 bg-white w-full flex justify-center">
 				<form
 					className="border-2 mt-8 w-2/6 px-2 rounded-xl flex items-center"
@@ -76,29 +80,94 @@ export default function SuppliersSearch() {
 				</form>
 			</div>
 
-			<div className="mt-24">
-				{items.map((item, i) => (
-					<div
-						key={item.uuid}
-						className="border-2 px-4 py-2 rounded-xl mt-1 flex items-center"
-					>
-						<img
-							src={`${localStorage.getItem("cdn_url")}${item.default_image.replace(/\\/g, "/")}`}
-							width={80}
-							height={80}
-							className="mr-2 my-1 rounded-lg"
-						/>
-						<div className="">
-							{item.article}
-							<br />
-							{item.name.replace(item.article, "").replace(item.brand, "")}{" "}
-							{item.brand}
-							<br />
-							{/*{item.description}*/}
-						</div>
-					</div>
+			<div className="mt-24 w-full px-3">
+				{items.map((item) => (
+					<Item {...item} key={item.article} />
 				))}
 			</div>
 		</main>
+	);
+}
+
+function Item({ article, name, brand, stocks, photo }: SupplierProduct) {
+	const { data: localProducts } = useProducts({
+		onlyInStock: true,
+	});
+	const localProduct = localProducts?.find(
+		(el) =>
+			el.code.toLowerCase().trim() === article.toLowerCase() ||
+			el.code.replace(/-/g, "").toLowerCase().trim() === article.toLowerCase(),
+	);
+	const [_, navigate] = useLocation();
+	return (
+		<div className="border-2 px-4 py-2 rounded-xl mt-1 flex w-full">
+			<img
+				src={`${localStorage.getItem("cdn_url")}${photo?.replace(/\\/g, "/")}`}
+				width={112}
+				height={112}
+				className="mr-2 my-1 rounded-lg object-cover h-24"
+			/>
+			<div className="w-3/6">
+				{article} <b>{brand}</b>
+				<br />
+				{name?.replace(article, "")?.replace(brand, "")}
+				<br />
+				{/*{item.description}*/}
+			</div>
+			<div className="flex">
+				<SupplierButton
+					name="Магазин"
+					price={localProduct?.price}
+					quantity={localProduct?.quantity}
+					onClick={() => {
+						// TODO: make it look ok
+						if (localProduct) {
+							useAppStore.setState({
+								searchValue: localProduct.searchCode,
+							});
+							navigate("/document/УТ-00000002/sell");
+						}
+					}}
+				/>
+				{Object.entries(stocks).map(([k, v]) => (
+					<SupplierButton
+						name={k}
+						price={(
+							v.price * Number(localStorage.getItem("markup") || 1)
+						).toFixed()}
+						quantity={v.quantity}
+					/>
+				))}
+			</div>
+		</div>
+	);
+}
+
+type SupplierButtonProps = {
+	name?: string;
+	price?: number | string;
+	quantity?: number | string;
+	onClick?: () => void;
+};
+
+function SupplierButton({
+	name,
+	price,
+	quantity,
+	onClick,
+}: SupplierButtonProps) {
+	return (
+		<button
+			onClick={onClick}
+			className="min-h-12 w-32 border-2 flex flex-col justify-center p-2 rounded-lg mx-1"
+		>
+			<span>{name}</span>
+			<span>Ціна: {price || 0}₴</span>
+			{Number(quantity) ? (
+				<span className="bg-green-300">Доступно {quantity}</span>
+			) : (
+				<span className="bg-red-200">Немає</span>
+			)}
+		</button>
 	);
 }
