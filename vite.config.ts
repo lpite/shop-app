d4e3260ee605af58ba71b819d510311095e4b73c
lpite <farionsasa@gmail.com>
2025-07-25 18:00:32 +0300

    up react version + use react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { visualizer } from "rollup-plugin-visualizer";
+// import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
+	],
 	define: {
 		"process.env.NODE_ENV": '"production"',
 		__BUILD_ID__: JSON.stringify(new Date().toLocaleString()),
