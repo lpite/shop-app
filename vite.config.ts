644e7a899363c08e0347b23d0be60cf8c11dbbc3
lpite <farionsasa@gmail.com>
2025-07-29 14:47:07 +0300

    use last commit as build id

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react-swc";
 import react from "@vitejs/plugin-react";
-
+import { execSync } from 'node:child_process'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
@@ -13,6 +13,6 @@ export default defineConfig({
 	],
 	define: {
 		"process.env.NODE_ENV": '"production"',
-		__BUILD_ID__: JSON.stringify(new Date().toLocaleString()),
+		__BUILD_ID__: JSON.stringify(execSync('git rev-parse --short HEAD').toString().trim()),
 	},
 });
