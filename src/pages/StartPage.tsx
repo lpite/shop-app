7c4f155b785738b2972b366119c323dffc0640c0
lpite <farionsasa@gmail.com>
2026-02-21 23:36:43 +0200

    add reports link to start page

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,16 +1,18 @@
-import { Settings } from "lucide-react";
+import { Settings, Sticker } from "lucide-react";
 import { Link } from "wouter";
 
 export default function StartPage() {
 	return (
 		<main className="flex items-center justify-center w-full h-full gap-4">
 			<span className="fixed top-3 start-3">{__BUILD_ID__}</span>
-			<Link
-				to="/config"
-				className="fixed end-3 top-3 p-2 hover:bg-slate-100 rounded-lg"
-			>
-				<Settings />
-			</Link>
+			<div className="fixed end-3 top-3 flex gap-3">
+				<Link to="/report" className="p-2 hover:bg-slate-100 rounded-lg">
+					<Sticker />
+				</Link>
+				<Link to="/config" className="p-2 hover:bg-slate-100 rounded-lg">
+					<Settings />
+				</Link>
+			</div>
 			<Link
 				to="/clients"
 				className="w-32 py-3 bg-sky-600 rounded-lg text-center text-white font-semibold"
