a36cb97acb586795bfd932a8cb50363172b0c8e3
lpite <farionsasa@gmail.com>
2026-03-19 19:18:28 +0200

    remove a bunch of useless code.

--- src/main.tsx
+++ src/main.tsx
@@ -15,22 +15,15 @@ import { OrderNotifier } from "./components/order-notification";
 import { ErrorBoundary } from "react-error-boundary";
 
 const PriceChanger = lazy(() => import("./pages/PriceChanger.tsx"));
-const Test2 = lazy(() => import("./pages/Test2.tsx"));
 const StatsPage = lazy(() => import("./pages/StatsPage.tsx"));
 const LeftOversReport = lazy(() => import("./pages/LeftoversReport.tsx"));
-const CategoryEditor = lazy(() => import("./pages/CategoryEditor.tsx"));
-const PosExperiment = lazy(() => import("./pages/PosExperiment.tsx"));
 const ReportsPage = lazy(() => import("./pages/Reports.tsx"));
 const PlaceChangeSuggestionReport = lazy(
 	() => import("./pages/PlaceChangeSuggestionReport.tsx"),
 );
-const SearchByVin = lazy(() => import("./pages/SearchByVin.tsx"));
-const FiltersDemo = lazy(() => import("./pages/filters-demo.tsx"));
-const SuppliersSearch = lazy(() => import("./pages/SuppliersSearch.tsx"));
 const IncomeDocumentHelper = lazy(
 	() => import("./pages/IncomeDocumentHelper.tsx"),
 );
-const TestPage = lazy(() => import("./pages/Test.tsx"));
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
@@ -51,10 +44,7 @@ createRoot(document.getElementById("root")!).render(
 				<Switch>
 					<Route path="/" component={StartPage} />
 					<Route path="/clients/" component={ClientSelectionPage} />
-					<Route path="/pos-new/:partnerId/:type" component={PosExperiment} />
 					<Route path="/pos/:partnerId/:type" component={PosPage} />
-					<Route path="/test" component={TestPage} />
-					<Route path="/test2/" component={Test2} nest />
 					<Route path="/price-changer/" component={PriceChanger} />
 					<Route
 						path="/stats/"
@@ -70,10 +60,6 @@ createRoot(document.getElementById("root")!).render(
 						path="/report/place-change-suggestions"
 						component={PlaceChangeSuggestionReport}
 					/>
-					<Route path="/category-editor/" component={CategoryEditor} />
-					<Route path="/suppliers-search/" component={SuppliersSearch} />
-					<Route path="/demo/" component={FiltersDemo} />
-					<Route path="/vin-demo/" component={SearchByVin} />
 					<Route path="/config/" component={ConfigPage} />
 					<Route
 						path="/document-helper/income"
