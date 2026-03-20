a4547f6991949897441af5b925cc4335fb42d814
lpite <farionsasa@gmail.com>
2026-03-20 13:45:53 +0200

    catalog api template

--- src/api/catalog.ts
+++ src/api/catalog.ts
@@ -3,11 +3,27 @@ type GetArticleDetails = {
 	article: string;
 };
 
+const catalog_url = "http://localhost:3000";
+
 async function getArticleDetails({ article, brand }: GetArticleDetails) {
-	console.log(article,brand)
-	return { oems: [], attributes: [] };
+	return fetch(
+		`${catalog_url}/catalog/brands/${brand}/articles/${article}`,
+	).then((r) => r.json());
 }
+
+function getCarBrands() {
+	return fetch(`${catalog_url}/brands`).then((r) => r.json());
+}
+
+function getCarModels(brandId: number) {
+	return fetch(`${catalog_url}/brands/${brandId}/models`).then((r) => r.json());
+}
+
+async function getProductBrands() {}
 
 export const catalog = {
 	getArticleDetails,
+	getCarBrands,
+	getCarModels,
+	getProductBrands,
 };
