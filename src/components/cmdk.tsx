198e8449263c7d27a96a157a1a4ac49d7f27c1fc
lpite <farionsasa@gmail.com>
2026-04-01 17:16:59 +0300

    improve price changer

---
+++ src/components/cmdk.tsx
@@ -0,0 +1,140 @@
+import {
+	Dispatch,
+	FormEvent,
+	SetStateAction,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+
+type CMDKProps = {
+	search: string;
+	onChangeSearch: Dispatch<SetStateAction<string>>;
+	isOpen: boolean;
+	onChangeOpen: Dispatch<SetStateAction<boolean>>;
+	items?: { id: string; name: string; onClick?: () => void }[];
+	onFormSubmit?: () => void;
+	closeOnSelect?: boolean;
+	title?: string;
+	isLoading?: boolean;
+};
+
+export function CMDK({
+	search,
+	onChangeSearch,
+	items = [],
+	isOpen,
+	onChangeOpen,
+	onFormSubmit,
+	closeOnSelect,
+	title,
+	isLoading,
+}: CMDKProps) {
+	const [focusedItem, setFocusedItem] = useState(0);
+	// щоб нормально достукуватися в обробнику натискань
+	const focusedItemRef = useRef(focusedItem);
+
+	const itemsBlockRef = useRef<HTMLDivElement>(null);
+
+	function onSubmit(e: FormEvent) {
+		e.preventDefault();
+		if (onFormSubmit) {
+			onFormSubmit();
+		}
+		const focused = items[focusedItem];
+		if (focused.onClick) {
+			focused.onClick();
+			if (closeOnSelect) {
+				onChangeOpen(false);
+			}
+		}
+	}
+
+	function arrowListener(event: KeyboardEvent) {
+		if (event.key === "ArrowDown") {
+			focusedItemRef.current = focusedItemRef.current + 1;
+			setFocusedItem((c) => c + 1);
+		}
+		if (event.key === "ArrowUp") {
+			if (focusedItemRef.current !== 0) {
+				focusedItemRef.current = focusedItemRef.current - 1;
+				setFocusedItem((c) => c - 1);
+			}
+		}
+		if (focusedItemRef.current >= 3 && itemsBlockRef.current) {
+			// завжди тримаємо сфокусований по центру
+			itemsBlockRef.current.scrollTo({
+				top: (focusedItemRef.current - 3) * 44,
+				behavior: "smooth",
+			});
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener("keydown", arrowListener);
+		return () => window.removeEventListener("keydown", arrowListener);
+	}, []);
+
+	return (
+		<>
+			<Dialog.Root open={isOpen} onOpenChange={onChangeOpen}>
+				<Dialog.Portal>
+					<Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-30" />
+					<Dialog.Content className="fixed w-4/6 bg-white shadow-lg top-1/2 start-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg">
+						<Dialog.Title className="sr-only">{title}</Dialog.Title>
+						<form
+							className="border-b-2 relative flex items-center"
+							onSubmit={onSubmit}
+						>
+							<input
+								className="w-full outline-none px-3 py-3 font-bold text-2xl duration-150 rounded-xl"
+								placeholder="Пошук..."
+								onChange={(e) => onChangeSearch(e.target.value)}
+								value={search}
+							/>
+							<button
+								type="button"
+								className="absolute right-2 p-2 rounded-lg hover:bg-black hover:bg-opacity-10"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									fill="currentColor"
+									viewBox="0 0 16 16"
+								>
+									<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+								</svg>
+							</button>
+						</form>
+						<div className="px-3 py-2 h-80 overflow-y-auto" ref={itemsBlockRef}>
+							{!items.length && !isLoading ? (
+								<div className="h-full flex items-center justify-center text-2xl">
+									Почни шукати
+								</div>
+							) : null}
+							{isLoading ? (
+								<div className="h-full flex items-center justify-center text-2xl">
+									Зачекай....
+								</div>
+							) : null}
+							{items.map((item, index) => (
+								<button
+									onClick={() => {
+										item.onClick ? item.onClick() : null;
+										closeOnSelect ? onChangeOpen(false) : null;
+									}}
+									key={item.id}
+									className={`text-xl text-start w-full px-2 py-2 hover:bg-black hover:bg-opacity-10 rounded ${focusedItem === index ? "bg-black !bg-opacity-20" : ""}`}
+								>
+									{item.name}
+								</button>
+							))}
+						</div>
+					</Dialog.Content>
+				</Dialog.Portal>
+			</Dialog.Root>
+		</>
+	);
+}
--- src/pages/PriceChanger.tsx
+++ src/pages/PriceChanger.tsx
@@ -1,10 +1,10 @@
 import useSWR from "swr";
-import { Dispatch, FormEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import useSWRMutation from "swr/mutation";
 
-import * as Dialog from "@radix-ui/react-dialog";
 import { useDebounce } from "@uidotdev/usehooks";
 import { fetcher } from "../utils/fetcher";
+import { CMDK } from "../components/cmdk";
 
 export default function PriceChanger() {
 	const [selRow, selRowf] = useState<number | undefined>(undefined);
@@ -70,7 +70,7 @@ export default function PriceChanger() {
 			fetcher({
 				url: `/shop/hs/app/product`,
 				method: "GET",
-			}) as Promise<{ name: string; searchCode: string }[]>,
+			}) as Promise<{ name: string; searchCode: string; code: string }[]>,
 		{ revalidateOnFocus: false },
 	);
 
@@ -119,14 +119,14 @@ export default function PriceChanger() {
 				closeOnSelect={true}
 				items={products
 					?.filter((el) =>
-						`${el?.name.toLowerCase()} ${el?.searchCode}`.includes(
+						`${el.name.toLowerCase()} ${el.searchCode} ${el.code}`.includes(
 							debouncedSearchValue.toLowerCase(),
 						),
 					)
 					.slice(0, 100)
 					.map((product) => ({
 						id: product.searchCode,
-						name: product.name,
+						name: `${product.searchCode} ${product.name}`,
 						onClick: () => setSelectedProduct(product),
 					}))}
 			/>
@@ -176,129 +176,5 @@ export default function PriceChanger() {
 				</table>
 			</div>
 		</main>
-	);
-}
-
-type CMDKProps = {
-	search: string;
-	onChangeSearch: Dispatch<React.SetStateAction<string>>;
-	isOpen: boolean;
-	onChangeOpen: Dispatch<React.SetStateAction<boolean>>;
-	items?: { id: string; name: string; onClick?: () => void }[];
-	onFormSubmit?: () => void;
-	closeOnSelect?: boolean;
-	title?: string;
-};
-
-function CMDK({
-	search,
-	onChangeSearch,
-	items = [],
-	isOpen,
-	onChangeOpen,
-	onFormSubmit,
-	closeOnSelect,
-	title,
-}: CMDKProps) {
-	const [focusedItem, setFocusedItem] = useState(0);
-	// щоб нормально достукуватися в обробнику натискань
-	const focusedItemRef = useRef(focusedItem);
-
-	const itemsBlockRef = useRef<HTMLDivElement>(null);
-
-	function onSubmit(e: FormEvent) {
-		e.preventDefault();
-		if (onFormSubmit) {
-			onFormSubmit();
-		}
-		const focused = items[focusedItem];
-		if (focused.onClick) {
-			focused.onClick();
-			if (closeOnSelect) {
-				onChangeOpen(false);
-			}
-		}
-	}
-
-	function arrowListener(event: KeyboardEvent) {
-		if (event.key === "ArrowDown") {
-			focusedItemRef.current = focusedItemRef.current + 1;
-			setFocusedItem((c) => c + 1);
-		}
-		if (event.key === "ArrowUp") {
-			if (focusedItemRef.current !== 0) {
-				focusedItemRef.current = focusedItemRef.current - 1;
-				setFocusedItem((c) => c - 1);
-			}
-		}
-		if (focusedItemRef.current >= 3 && itemsBlockRef.current) {
-			// завжди тримаємо сфокусований по центру
-			itemsBlockRef.current.scrollTo({
-				top: (focusedItemRef.current - 3) * 44,
-				behavior: "smooth",
-			});
-		}
-	}
-
-	useEffect(() => {
-		window.addEventListener("keydown", arrowListener);
-		return () => window.removeEventListener("keydown", arrowListener);
-	}, []);
-
-	return (
-		<>
-			<Dialog.Root open={isOpen} onOpenChange={onChangeOpen}>
-				<Dialog.Portal>
-					<Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-30" />
-					<Dialog.Content className="fixed w-4/6 bg-white shadow-lg top-1/2 start-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg">
-						<Dialog.Title className="sr-only">{title}</Dialog.Title>
-						<form
-							className="border-b-2 relative flex items-center"
-							onSubmit={onSubmit}
-						>
-							<input
-								className="w-full outline-none px-3 py-3 font-bold text-2xl duration-150 rounded-xl"
-								placeholder="Пошук..."
-								onChange={(e) => onChangeSearch(e.target.value)}
-								value={search}
-							/>
-							<button
-								type="button"
-								className="absolute right-2 p-2 rounded-lg hover:bg-black hover:bg-opacity-10"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									fill="currentColor"
-									viewBox="0 0 16 16"
-								>
-									<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-								</svg>
-							</button>
-						</form>
-						<div className="px-3 py-2 h-80 overflow-y-auto" ref={itemsBlockRef}>
-							{!items.length ? (
-								<div className="h-full flex items-center justify-center text-2xl">
-									Почни шукати
-								</div>
-							) : null}
-							{items.map((item, index) => (
-								<button
-									onClick={() => {
-										item.onClick ? item.onClick() : null;
-										closeOnSelect ? onChangeOpen(false) : null;
-									}}
-									key={item.id}
-									className={`text-xl text-start w-full px-2 py-2 hover:bg-black hover:bg-opacity-10 rounded ${focusedItem === index ? "bg-black !bg-opacity-20" : ""}`}
-								>
-									{item.name}
-								</button>
-							))}
-						</div>
-					</Dialog.Content>
-				</Dialog.Portal>
-			</Dialog.Root>
-		</>
 	);
 }
